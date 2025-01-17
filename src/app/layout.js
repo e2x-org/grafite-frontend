5586456a3c7e02ad46207496d2707b89a8713e0d
natyavidhan <natyavidhanbiswas10@gmail.com>
2025-01-17 08:01:40 +0530

    theme and font fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,28 @@
 import { Gowun_Dodum } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider"
+
 import "./globals.css";
 
 const gowun_dodum = Gowun_Dodum({
-  variable: "--font-gowun_dodum",
-  subsets: ["latin"],
-  weight: ['400']
+	variable: "--font-gowun_dodum",
+	subsets: ["latin"],
+	weight: ['400']
 });
 
 
 export const metadata = {
-  title: "grafite. | home",
-  description: "sometimes all it takes is a shit ton of data and some coffee.",
+	title: "grafite. | home",
+	description: "sometimes all it takes is a shit ton of data and some coffee.",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${gowun_dodum.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${gowun_dodum.variable} antialiased`}>
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem >
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="text-center mt-10">
-			<h1 className="text-2xl">grafite.</h1>
+			<h1 className="text-6xl">grafite.</h1>
+			<h1>sometimes all it takes is a shit ton of data and some coffee.</h1>
 		</div>
 	);
 }
