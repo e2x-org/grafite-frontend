21fef97bc566b0717fd582debda38fb4e0324a5c
natyavidhan <natyavidhanbiswas10@gmail.com>
2025-01-18 09:17:03 +0530

    dashboard thing

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/dashboard.css'
+
+function Dashboard() {
+    return (
+        <div className="container">
+            Welcome
+        </div>
+    )
+}
+
+export default Dashboard
